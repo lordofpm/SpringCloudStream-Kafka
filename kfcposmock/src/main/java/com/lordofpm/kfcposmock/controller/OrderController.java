417feb3de165e6aa3b8668424d68d4e16abf1259
Eric Zhang <lordofpm@163.com>
2018-06-03 13:08:47 +0800

    Check in KFC code.

--- kfcposmock/src/main/java/com/lordofpm/kfcposmock/controller/OrderController.java
+++ kfcposmock/src/main/java/com/lordofpm/kfcposmock/controller/OrderController.java
@@ -5,7 +5,9 @@ package com.lordofpm.kfcposmock.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lordofpm.coreschema.domain.Order;
@@ -18,9 +20,9 @@ import com.lordofpm.coreschema.domain.Order;
 public class OrderController {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-	@RequestMapping(path="/country")
-	public String postOrderToPOS(Order order) {
-		logger.info("Get and processed order successfully and order id is " + order.getId());
+	@RequestMapping(path = "/${location.country}/posprocessor", method = RequestMethod.POST)
+	public String postOrderToPOS(@RequestBody Order order) {
+		logger.info("Got and processed order successfully and order id is " + order.getId());
 		return "success";
 	}
 }
